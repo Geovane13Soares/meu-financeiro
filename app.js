a7c80aec7c47a0735222e6b1ccd3c8ad88f34ede
Geovane13Soares <geovane131@gmail.com>
2026-04-17 15:58:32 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+let telaIndex = 0;
+const telas = ["home", "lancamentos", "busca", "backup"];
+
+// Trocar tela
+function trocarTela(nome) {
+  telaIndex = telas.indexOf(nome);
+  
+  document.getElementById("telas").style.transform =
+    `translateX(-${telaIndex * 100}%)`;
+  
+  atualizarMenu();
+  atualizar();
+}
+
+// Atualizar botão ativo
+function atualizarMenu() {
+  document.querySelectorAll(".menu button").forEach(btn => {
+    btn.classList.remove("ativo");
+  });
+  
+  document.getElementById("btn-" + telas[telaIndex]).classList.add("ativo");
+}
+
+// SWIPE (arrastar dedo)
+let startX = 0;
+
+document.getElementById("telas").addEventListener("touchstart", e => {
+  startX = e.touches[0].clientX;
+});
+
+document.getElementById("telas").addEventListener("touchend", e => {
+  let endX = e.changedTouches[0].clientX;
+  let diff = startX - endX;
+  
+  if (diff > 50 && telaIndex < telas.length - 1) {
+    telaIndex++;
+  }
+  
+  if (diff < -50 && telaIndex > 0) {
+    telaIndex--;
+  }
+  
+  trocarTela(telas[telaIndex]);
+});
+
+// iniciar
+atualizarMenu();
